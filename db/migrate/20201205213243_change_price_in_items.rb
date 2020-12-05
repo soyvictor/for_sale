bfc5b07d49bbb29eb5acdbc954a8d6f4c62b298f
Victor Chiang <vgchiang@gmail.com>
2020-12-05 16:29:53 -0600

    properly sized image from cloudinary and styled homepage boxes

--- db/migrate/20201205213243_change_price_in_items.rb
+++ db/migrate/20201205213243_change_price_in_items.rb
@@ -1,5 +1,5 @@
 class ChangePriceInItems < ActiveRecord::Migration[6.0]
   def change
-    change_column :items, :price, :float
+    change_column :items, :price, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_05_043507) do
+ActiveRecord::Schema.define(version: 2020_12_05_213243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2020_12_05_043507) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
-    t.float "price"
+    t.integer "price"
     t.boolean "sold", default: false, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
