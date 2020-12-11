9a16a082806b1de024d890d96e41ef4f432ca002
Victor Chiang <vgchiang@gmail.com>
2020-12-11 13:40:49 -0600

    progress on thumbnail loop

--- app/javascript/packs/carousel.js
+++ app/javascript/packs/carousel.js
@@ -1,18 +1,29 @@
-console.log('hi from carousel!');
 const thumbnails = document.querySelectorAll('img.thumbnail');
 const carouselImages = document.querySelectorAll('.carousel-item');
+const thumbnailFirst = document.querySelector('.thumbnail');
+const thumbnailAll = document.querySelectorAll('.thumbnail');
+const carouselSlider = document.querySelector('.carousel-slider');
+const nextButton = document.querySelector('.carousel-control-next-icon');
+console.log(nextButton);
+const prevButton = document.querySelector('.carousel-control-prev-icon');
+
+
+thumbnailFirst.classList.add('highlightOrange');
+// console.log(carouselImageActive);
 
 const highlightOrange = (e) => {
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.classList.remove('highlightOrange');
+  })
   const imageNumber = Number.parseInt(e.target.dataset.count);
   e.target.classList.add('highlightOrange');
-  console.log(carouselImages[0]);
   carouselImages.forEach((image) => {
     image.classList.remove('active');
   });
   carouselImages[`${imageNumber}`].classList.add('active');
 
 
-  e.target.addEventListener('mouseout', () => e.target.classList.remove('highlightOrange'));
+  // e.target.addEventListener('mouseout', () => e.target.classList.remove('highlightOrange'));
   // const carouselImg = document.querySelectorAll(".carousel-img");
   // console.log(carouselImg);
 };
@@ -20,3 +31,20 @@ const highlightOrange = (e) => {
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener('mouseover', highlightOrange);
 });
+
+const highlightThumbnail = () => {
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.classList.remove('highlightOrange')
+  });
+  const carouselImageActive = document.querySelector('.carousel-item.active > img');
+  const activeImageNumber = Number.parseInt(carouselImageActive.dataset.count, 10);
+  console.log(activeImageNumber);
+
+  thumbnailAll[`${activeImageNumber}`].classList.add('highlightOrange');
+
+
+};
+
+
+nextButton.addEventListener('click', highlightThumbnail);
+
