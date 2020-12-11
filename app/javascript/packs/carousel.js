46577042ff1cee17335d5160857509bbbbd2e5e1
Victor Chiang <vgchiang@gmail.com>
2020-12-11 14:07:54 -0600

    carousel and thumbnails are in sync now

--- app/javascript/packs/carousel.js
+++ app/javascript/packs/carousel.js
@@ -2,10 +2,10 @@ const thumbnails = document.querySelectorAll('img.thumbnail');
 const carouselImages = document.querySelectorAll('.carousel-item');
 const thumbnailFirst = document.querySelector('.thumbnail');
 const thumbnailAll = document.querySelectorAll('.thumbnail');
-const carouselSlider = document.querySelector('.carousel-slider');
 const nextButton = document.querySelector('.carousel-control-next-icon');
-console.log(nextButton);
 const prevButton = document.querySelector('.carousel-control-prev-icon');
+const thumbnailLength = document.querySelectorAll('.thumbnail').length;
+let thumbnailCount = 0;
 
 
 thumbnailFirst.classList.add('highlightOrange');
@@ -21,30 +21,40 @@ const highlightOrange = (e) => {
     image.classList.remove('active');
   });
   carouselImages[`${imageNumber}`].classList.add('active');
-
-
-  // e.target.addEventListener('mouseout', () => e.target.classList.remove('highlightOrange'));
-  // const carouselImg = document.querySelectorAll(".carousel-img");
-  // console.log(carouselImg);
+  thumbnailCount = imageNumber;
 };
 
 thumbnails.forEach((thumbnail) => {
   thumbnail.addEventListener('mouseover', highlightOrange);
 });
 
-const highlightThumbnail = () => {
+const highlightThumbnailNext = () => {
   thumbnails.forEach((thumbnail) => {
     thumbnail.classList.remove('highlightOrange')
   });
-  const carouselImageActive = document.querySelector('.carousel-item.active > img');
-  const activeImageNumber = Number.parseInt(carouselImageActive.dataset.count, 10);
-  console.log(activeImageNumber);
+  if (thumbnailCount < (thumbnailLength - 1) ) {
+    thumbnailAll[`${thumbnailCount + 1}`].classList.add('highlightOrange');
+    thumbnailCount += 1;
+  } else {
+    thumbnailAll[0].classList.add('highlightOrange');
+    thumbnailCount = 0;
+  }
+};
 
-  thumbnailAll[`${activeImageNumber}`].classList.add('highlightOrange');
-
-
+const highlightThumbnailPrev = () => {
+  thumbnails.forEach((thumbnail) => {
+    thumbnail.classList.remove('highlightOrange')
+  });
+  if (thumbnailCount == 0) {
+    thumbnailAll[`${thumbnailLength - 1}`].classList.add('highlightOrange');
+    thumbnailCount = thumbnailLength - 1;
+  } else {
+    thumbnailAll[`${thumbnailCount - 1}`].classList.add('highlightOrange');
+    thumbnailCount -= 1;
+  }
 };
 
 
-nextButton.addEventListener('click', highlightThumbnail);
+nextButton.addEventListener('click', highlightThumbnailNext);
+prevButton.addEventListener('click', highlightThumbnailPrev);
 
